aeef21a7eed2be04e66903f38558af2756a5ef32
Diego <diegoulloao@icloud.com>
2022-12-10 11:57:09 -0300

    feat: deployment.yaml environment variables system inside deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,16 +3,16 @@
 # load project environment variables
 set -a; source .env; set +a
 
-# image name
-export IMAGE_NAME=quake3
-
 # ----------------------------------
+
+# get last commit hash
+COMMIT_HASH=$(git log --pretty=format:'%h' -n 1)
 
 # authentication
 gcloud config set project $PROJECT_ID
 
 # build docker image (linux arm64 specific)
-docker build --platform=linux/amd64 -t $IMAGE_NAME .
+docker build --platform=linux/amd64 -t quake3 .
 
 # upload image to the cloud register
 gcloud auth configure-docker
@@ -22,9 +22,20 @@ docker push gcr.io/$PROJECT_ID/$CONTAINER_IMAGE
 # connect to k8s to install image
 gcloud container clusters get-credentials $CLUSTER --zone $ZONE --project $PROJECT_ID
 
-# install
-kubectl apply -f deployment.yaml
+# copy of deployment.yaml replacing the hash value
+sed -e "s/\${COMMIT_HASH}/$COMMIT_HASH/; \
+  s/\${PROJECT_ID}/$PROJECT_ID/; \
+  s/\${CONTAINER_IMAGE}/$CONTAINER_IMAGE/" \
+deployment.yaml > .deployment.tmp.yaml
+
+# apply copy of deployment.yaml with commit hash inside
+kubectl apply -f .deployment.tmp.yaml
+
+# apply services
 kubectl apply -f services.yaml
 
-# print clusters info
-kubectl get svc $IMAGE_NAME
+# delete copy of deployment.yaml
+rm .deployment.tmp.yaml
+
+# print cluster info
+kubectl get svc quake3
